696b249b5bbb5afc214eebc93fb653fb885d8ef4
Robiul Islam Forhad <139230850+robiul-512@users.noreply.github.com>
2023-09-26 16:35:38 +0600

    Create Word Capitalization.cpp

---
+++ LAB-06/Word Capitalization.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+
+
+using namespace std;
+
+int main(){
+
+    string name;
+    cin>>name;
+
+    char first=toupper(name[0]);
+    name[0]=first;
+    cout<<name<<endl;
+
+return 0;

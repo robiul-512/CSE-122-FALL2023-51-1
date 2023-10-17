1abb5fa1b0f745c57abd42dccced38090be22646
Robiul Islam Forhad <139230850+robiul-512@users.noreply.github.com>
2023-10-17 16:56:54 +0600

    Create Tram.cpp

---
+++ lab-09/Tram.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+int n,in=0,out=0,max=0;
+cin>>n;
+for(int i=0;i<n;i++){
+int x,y;
+cin>>x>>y;
+in=in-x;
+in=in+y;
+if(max<in)
+max=in;
+}
+cout<<max<<endl;
+
+   return 0;
+}
